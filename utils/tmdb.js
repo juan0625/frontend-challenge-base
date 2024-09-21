686c45e74abf092c708aa3c836bb7b074f7f6d0f
Juan David Monsalve Ortiz <juan@enternova.com.co>
2024-09-21 15:53:13 -0500

    Integración con el backend usuarios.

--- utils/tmdb.js
+++ utils/tmdb.js
@@ -29,3 +29,13 @@ export const searchMovies = async (query) => {
     return data;
   };
   
+import axios from 'axios';
+
+const fetchUsers = async () => {
+  try {
+    const response = await axios.get('/api/users');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching users', error);
+  }
+};
